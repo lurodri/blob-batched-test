f12e682172c61829de9b770af55444df38df2844
Luciano Rodrigues <lurodri@microsoft.com>
2021-09-16 17:58:24 -0300

    Adding event hub consumer

---
+++ event-hub-consumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace event_hub_consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
